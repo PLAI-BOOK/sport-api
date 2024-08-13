5b9a3ef91fe9135527d5abda515599759a32c722
manuelruff <manuel42994299@gmail.com>
2024-08-13 17:23:19 +0300

    base

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sport-api
+
+you need to create .env and put in 
+API_KEY
+BASE_URL
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import http.client
 import requests
 import json
+import sys
 from pprint import pprint
+from dotenv import load_dotenv
+import os
 
+
+# this is a demo for calling the api and saving the response to a file
+# the demo uses leagues?season=2023 as a demo, you can change that to whatever endpoint you want to call!
+# it will save the response to a file with a dynamic name based on the endpoint called
+
+# Ensure stdout is UTF-8 encoded
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
+# Load the .env file
+load_dotenv()
 # test
-api_key = "25a0d1c185e936e0e116132504637d1c"
-base_url = "v3.football.api-sports.io"
-
-print(api_key)
-
+api_key =os.getenv('API_KEY')
+base_url = os.getenv('BASE_URL')
 
 conn = http.client.HTTPSConnection(base_url)
 
 headers = {
     'x-rapidapi-host': base_url,
     'x-rapidapi-key': api_key
-    }
+}
 
 # Adding parameters to the request (e.g., for a specific season and current leagues)
 params = "/leagues?season=2023"
@@ -25,16 +34,15 @@ conn.request("GET", params, headers=headers)
 res = conn.getresponse()
 data = res.read()
 
-# Pretty print the JSON response
+# Decode the JSON response
 response_data = json.loads(data.decode("utf-8"))
+
+# Use pprint to print the JSON response to the console in a formatted way
 pprint(response_data)
 
-url = "https://v3.football.api-sports.io/leagues?season=2023"
+# create good filename
+safe_filename = f"{base_url}{params.replace('?', '_').replace('&', '_').replace('=', '-').replace('/', '-')}.json"
 
-payload={}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-# Pretty print the JSON response from the second request
-response_data = response.json()
-pprint(response_data)
+# Save the JSON response to a file with a dynamic name
+with open(safe_filename, 'w', encoding='utf-8') as file:
+    json.dump(response_data, file, ensure_ascii=False, indent=4)
