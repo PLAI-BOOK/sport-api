1aac4b5e206b4f6c76be1f6415becabeb2f330a9
Manuel Ruffieux <manuel42994299@gmail.com>
2024-08-22 22:34:42 +0300

    fixture statistics up

--- fixtures_statistics/fixtures_statistics.py
+++ fixtures_statistics/fixtures_statistics.py
@@ -2,5 +2,5 @@ from connection import call_api
 
 if __name__=="__main__":
     # Specify the API endpoint, headers, and directory to save the file
-    params = "/fixtures?live=all"  # Change as needed
+    params = "/fixtures/statistics?fixture=1216981"  # Change as needed
     call_api(params)
